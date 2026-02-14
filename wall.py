ab8c5abf136b04a48b6bd21492d25a80a96f96ff
Hasan-cas <180566870+Hasan-cas@users.noreply.github.com>
2026-02-14 16:53:15 +0600

    Update wall.py

--- wall.py
+++ wall.py
@@ -83,8 +83,8 @@ def setup_security():
     csp = {
         'default-src': ["'self'"],
         'img-src': ["'self'"] + TARGET_URLS,
-        'style-src': ["'self'", "'unsafe-inline'"] + TARGET_URLS,
-        'script-src': ["'self'", "'unsafe-inline'"] + TARGET_URLS,
+        'style-src': ["'self'"] + TARGET_URLS,
+        'script-src': ["'self'"] + TARGET_URLS,
         'connect-src': ["'self'", "wss://*.facebook.com"] + TARGET_URLS,
         'font-src': ["'self'"] + TARGET_URLS,
         'object-src': ["'none'"],
